1f535015e461c43e78b98272f58034600fe4affb
Lauren DiGiovanni <lad475@nyu.edu>
2017-10-18 13:10:08 -0400

    Added sketch.js

---
+++ sketch.js
@@ -0,0 +1,14 @@
+/*
+	The main JavaScript file to run our Virtual pet
+	Contains the draw function, which is what prints to the screen
+*/
+
+function preload(){
+
+}
+function setup(){
+
+}
+function draw(){
+
+}
