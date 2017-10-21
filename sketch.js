fbdce2c04f90e08c0ab63b7e52e10acd5bc37fe6
Lauren DiGiovanni <lad475@nyu.edu>
2017-10-20 22:58:37 -0400

    Added pet object and templated for methods and expected needed fields

--- sketch.js
+++ sketch.js
@@ -7,8 +7,73 @@ function preload(){
 
 }
 function setup(){
-
+	myCanvas = createCanvas(750, 750);
+	myCanvas.parent("sketch-holder");
 }
 function draw(){
 
+}
+//This will be the pet object that the user must raise
+function Character(n){
+	this.name = n;
+	//Represents health of the user's pet
+	this.health = 50;
+	//Represents how hungry the pet is: 0 is very hungry, 10 is full.
+	this.hunger = 0;
+	//Represents how clean the pet is: the higher the number, the dirtier the pet
+	this.dirty = 0;
+	//Amount of love given to pet: can go negative
+	this.love = 0;
+	//Age measured in hours, minutes, and days
+	this.age = {
+		hours: 0,
+		minutes: 0,
+		days: 0
+	};
+	//The array of images that represent the current character
+	this.imgArr = [];
+	//The stage is similar to the age, but is changed at specific ages.  0 is the baby stage
+	this.stage = 0;
+	//Happiness variable: 0 is least happy to 10 most happy
+	this.happiness = 0;
+
+
+	this.display = function(){
+		//Later on, display pet
+	};
+	this.sayHi = function(){
+		//Implement to say hi when the user speaks the words "hello" or "hi" into the mic
+	};
+	this.updateAge = function(){
+		//Implement a method to take in the time and change the age of the pet
+	};
+	this.changeStage = function(){
+		//Using the health as one variable, and another later implemented
+		//"good care" variable, change the stage to the appropriate character
+	};
+	this.updateImageArray = function(){
+		//Update the array of images that represents this character
+		//Typically called when the stage of the character changes
+		//Later on implement the arrays
+	};
+	this.giveLove = function(){
+		//Add to the pet's love variable
+	};
+	this.giveHate = function(){
+		//Decrement the love variable
+	};
+	this.feed = function(){
+		//Increment the hunger variable
+		//Possibly give love if the food is good
+		//Possibly take love if the food is disliked by this character
+	};
+	this.makeHappy = function(){
+		//Increment the happiness
+	};
+	this.makeSad = function(){
+		//Decrement happiness
+	};
+	this.makeHungry = function(){
+		//Decrement hunger
+	};
 }
