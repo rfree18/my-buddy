277fde51e61b5d7f944edc3ba5abdedd3281ab36
Ross Freeman <rfreeman1@me.com>
2017-12-11 23:04:21 -0500

    Code cleanup

--- sketch/ar_sketch.js
+++ sketch/ar_sketch.js
@@ -31,24 +31,6 @@ function setup() {
     markers.push(world.getMarker(tag.id));
   });
 
-  // create some geometry to add to our marker
-  // the marker is 1 meter x 1 meter, with the origin at the center
-  // the x-axis runs left and right
-  // -0.5, 0, -0.5 is the top left corner
-  // var littleCube1 = new Box({
-  //   x: 0,
-  //   y: 0.25,
-  //   z: 0,
-  //   red: 255,
-  //   green: 0,
-  //   blue: 0,
-  //   width: 0.5,
-  //   height: 0.5,
-  //   depth: 0.5,
-  //   asset: 'stonebrick'
-  // });
-  // markerHiro.addChild(littleCube1);
-
 }
 
 
@@ -59,7 +41,7 @@ function draw() {
         myChar.properties.unlockables = {};
       }
 
-      if(myChar.properties.unlockables[marker.tag.id]) {
+      if (myChar.properties.unlockables[marker.tag.id]) {
         window.location = 'index.html';
       } else {
         isLoading = true;
