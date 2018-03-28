98dbaf9306ae394e78ead8ba4c38f266a659c488
Ross Freeman <rfreeman1@me.com>
2018-03-28 18:59:50 +0200

    Fix buttons

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,15 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
+    isGame: true,
     encodedJson: encodeURIComponent(JSON.stringify(req.user))
   });
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', {});
+  res.render('about', {
+    encodedJson: encodeURIComponent(JSON.stringify(req.user))
+  });
 });
 
 module.exports = router;
